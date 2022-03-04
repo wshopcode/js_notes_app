8c6c1d98bba81d743fd5cc1b793c8d831dbce5c2
wshopcode <99200275+wshopcode@users.noreply.github.com>
2022-03-04 13:29:36 +0300

    Delete notes_app directory

--- notes_app/js/main.js
+++
@@ -1,9 +0,0 @@
- import NotesAPI from "./NotesAPI.js"
-
- NotesAPI.saveNotes({
-    id: 828588,
-    title: "The tile has changed!",
-    body: "Napster things."
- });
-
- console.log(NotesAPI.getALLNotes());
